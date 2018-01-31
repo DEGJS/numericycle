f836fcdc3a5cd45613c0a4122291851f159fb2d6
Ryan Heap <rheap@degdigital.com>
2018-01-31 09:35:12 -0600

    removed and upgraded dependencies, included JSPM config file

--- karma.conf.js
+++ karma.conf.js
@@ -39,9 +39,7 @@ module.exports = function(config) {
             paths: {
                 'babel': 'node_modules/babel-core/browser.js',
                 'systemjs': 'node_modules/systemjs/dist/system.js',
-                'system-polyfills': 'node_modules/systemjs/dist/system-polyfills.js',
-                'es6-module-loader': 'node_modules/es6-module-loader/dist/es6-module-loader.js',
-                'phantomjs-polyfill': 'node_modules/phantomjs-polyfill/bind-polyfill.js'
+                'system-polyfills': 'node_modules/systemjs/dist/system-polyfills.js'
             }
         }
     },
